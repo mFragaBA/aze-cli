e0f533dacded0b1056eacf0cce21a36568ea5d37
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-06-25 12:59:02 +0530

    beautify peek hand cards

--- cli/src/peek_hand.rs
+++ cli/src/peek_hand.rs
@@ -1,6 +1,6 @@
 use aze_lib::client::{ create_aze_client, AzeClient };
 use aze_lib::constants::{ PLAYER_CARD1_SLOT, PLAYER_CARD2_SLOT, PLAYER_FILE_PATH };
-use aze_lib::utils::Player;
+use aze_lib::utils::{ card_from_number, Player };
 use clap::Parser;
 use miden_objects::{ 
     accounts::AccountId,
@@ -21,8 +21,12 @@ impl PeekHandCmd {
         let mut client: AzeClient = create_aze_client();
         let (player_account, _) = client.get_account(sender_account_id).unwrap();
         for (i, slot) in (PLAYER_CARD1_SLOT..PLAYER_CARD2_SLOT + 1).enumerate() {
-            let card = player_account.storage().get_item(slot);
-            println!("Card {}: {:?}", i + 1, card);
+            let card_digest = player_account.storage().get_item(slot);
+            let card_suit = card_digest.as_elements()[0].as_int();
+            let card_rank = card_digest.as_elements()[1].as_int();
+            let card_number: u64 = (card_suit - 1) * 13 + card_rank;
+            let card = card_from_number(card_number);
+            println!("Card {}: {}", i + 1, card);
         }
 
         Ok(())
