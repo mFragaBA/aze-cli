d9f951c0da13c44e496669d3b351d6f13ac110c9
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-06-26 13:26:43 +0530

    supress note incompatible error

--- lib/src/executor/mod.rs
+++ lib/src/executor/mod.rs
@@ -1,7 +1,9 @@
 use crate::client::AzeClient;
 use miden_client::{
     client::transactions::transaction_request::TransactionRequest, store::TransactionFilter,
+    errors::ClientError
 };
+use miden_tx::{ TransactionExecutorError, TransactionCompilerError };
 
 pub async fn execute_tx_and_sync(client: &mut AzeClient, tx_request: TransactionRequest) {
     let _ = match client.sync_state().await {
@@ -14,9 +16,17 @@ pub async fn execute_tx_and_sync(client: &mut AzeClient, tx_request: Transaction
     let transaction_execution_result = match client.new_transaction(tx_request.clone()) {
         Ok(result) => result,
         Err(e) => {
-            // suppressing the error 
-            // println!("Error creating transaction: {:?}", e);
-            return;
+            match e {
+                ClientError::TransactionExecutorError(TransactionExecutorError::CompileTransactionFailed(TransactionCompilerError::NoteIncompatibleWithAccountInterface(_))) => {
+                    // Suppress this specific error
+                    return;
+                }
+                _ => {
+                    // Handle other errors
+                    println!("Error creating transaction: {:?}", e);
+                    return;
+                }
+            };
         }
     };
     let transaction_id = transaction_execution_result.executed_transaction().id();
